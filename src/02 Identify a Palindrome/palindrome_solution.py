eef264861cd0325b4bca7080c30abe820b1e781d
cryptozero <32773449+cryptozero@users.noreply.github.com>
2023-02-22 23:35:40 +0000

    refactor and fix palindrome solution

--- src/02 Identify a Palindrome/palindrome_solution.py
+++ src/02 Identify a Palindrome/palindrome_solution.py
@@ -1,12 +1,9 @@
 def is_palindrome(phrase: str):
     phrase_letters = "".join([c for c in phrase.lower() if c.isalpha()])
 
-    if phrase_letters == reversed(phrase_letters):
-        return True
-    else:
-        return False
+    return phrase_letters == phrase_letters[::-1]
 
 
-if __name__ == "main":
-    assert is_palindrome("racecar")
+if __name__ == "__main__":
+    assert is_palindrome("race car")
     assert not is_palindrome("hola")
